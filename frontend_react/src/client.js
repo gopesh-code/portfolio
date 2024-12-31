bb45cf196367c49d4c2286d7bdae6c44c53cb07e
Gopesh <gopeshkumarpathak@gmail.com>
2024-12-31 10:23:40 +0530

    updation

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,14 +1,21 @@
+// client.js
 import { createClient } from '@sanity/client';
-import imageUrlBuilder from "@sanity/image-url";
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: "production",
-  apiVersion: "2023-08-13",
+  dataset: 'production',
+  apiVersion: '2023-08-13',
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
 
-export const urlFor = (source) => builder.image(source);
+export const urlFor = (source) => {
+  if (!source || !source.asset) {
+    console.error('Invalid image source:', source);
+    return '/path-to-placeholder-image.jpg'; // Fallback to a placeholder image
+  }
+  return builder.image(source);
+};
